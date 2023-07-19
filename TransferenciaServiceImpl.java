3b317a2b3271120b45911c4629cb84b35439d01a
Rafael Carlos <86086827+Rafallt@users.noreply.github.com>
2023-07-18 22:19:07 -0300

    TransferenciaServiceImpl.java
    Implementação da Classe de Serviço

---
+++ TransferenciaServiceImpl.java
@@ -0,0 +1,13 @@
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class TransferenciaServiceImpl implements TransferenciaService {
+    private final TransferenciaRepository transferenciaRepository;
+
+    // Injete o repositório no construtor
+
+    // Implemente os métodos da interface utilizando o repositório
+}
