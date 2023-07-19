69ba655d0a02cd64267cceb1b645759fc3bff89c
Rafael Carlos <86086827+Rafallt@users.noreply.github.com>
2023-07-18 22:20:10 -0300

    TransferenciaController.java
    Controlador REST

---
+++ TransferenciaController.java
@@ -0,0 +1,14 @@
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/transferencias")
+public class TransferenciaController {
+    private final TransferenciaService transferenciaService;
+
+    // Injete o serviço no construtor
+
+    // Defina os endpoints da API para os diferentes filtros de busca
+}
