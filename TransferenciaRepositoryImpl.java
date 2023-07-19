6634a1d78515fce91c58bf2322b384aa2f87ba67
Rafael Carlos <86086827+Rafallt@users.noreply.github.com>
2023-07-18 22:17:12 -0300

    TransferenciaRepositoryImpl.java
    Implementação do Repositório com dados em memória

---
+++ TransferenciaRepositoryImpl.java
@@ -0,0 +1,11 @@
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class TransferenciaRepositoryImpl implements TransferenciaRepository {
+    private List<Transferencia> transferencias = new ArrayList<>();
+
+    // Implemente os métodos da interface utilizando a lista "transferencias"
+}
